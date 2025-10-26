983636f699ff8ae3f165eda602405b84780d7e1a
Ben P. <benj.c.palmer@gmail.com>
2025-10-26 12:30:49 -0400

    Point at swiftlang github org instead of apple for the swift-syntax dependency

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", .upToNextMajor(from: "601.0.1"))
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", .upToNextMajor(from: "601.0.1"))
     ],
     targets: [
         .macro(
